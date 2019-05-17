52421ee9b2e169782455d652a5c04abcab06fbc2
Yordan Galabov <galabovj@gmail.com>
2019-05-17 19:12:22 +0300

    initialize react app with phaser

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PhaserContainer from "./PhaserContainer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PhaserContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
